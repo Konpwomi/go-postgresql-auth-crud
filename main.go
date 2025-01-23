acb08a130f5a58de069234f6d386b45d12a926a7
Konpwomi <patiphan.arp@gmail.com>
2025-01-23 15:23:52 +0700

    update: using go fiber

--- main.go
+++ main.go
@@ -1,35 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"fmt"
+	"github.com/gofiber/fiber/v2"
 )
 
-// func helloHandler(w http.ResponseWriter, r *http.Request) {
-// 	if r.Method == http.MethodGet {
-// 		fmt.Fprintln(w, "GET request received")
-// 	} else if r.Method == http.MethodPost {
-// 		fmt.Fprintln(w, "POST request received")
-// 	} else {
-// 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-// 	}
-// }
-
-func jsonHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"message": "Hello, JSON!",
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
-}
-
-// func aboutHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintln(w, "About Page")
-// }
 
 func main() {
-	http.HandleFunc("/json", jsonHandler)
-	fmt.Println("Server run at port : 8080")
-	http.ListenAndServe(":8080", nil)
+	app := fiber.New()
+	app.Get("/", func (c *fiber.Ctx) error  {
+		return c.SendString("Hello World")
+	})
+	app.Listen(":8080")
 } 
